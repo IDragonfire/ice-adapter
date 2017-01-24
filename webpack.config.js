993416affab5c9a92ed3d3a6e14342e04da1d322
Dragonfire <bastian616-git@yahoo.de>
2017-01-24 19:14:25 +0100

    Basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {  
+  entry: './index.ts',
+  output: {
+    filename: 'faf-ice-adapter.js'
+  },
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+  },
+  module: {
+    loaders: [
+      { test: /\.ts$/, loader: 'ts-loader' }
+    ]
+  }
+}
