811d67a2478eae3d23de94f7ed480e2f54ebd09b
Dragonfire <bastian616-git@yahoo.de>
2017-01-25 13:45:13 +0100

    exclude node modules http://jlongster.com/Backend-Apps-with-Webpack--Part-I

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,15 @@
-var nodeExternals = require('webpack-node-externals');
+var webpack = require("webpack");
+var path = require("path");
+var fs = require("fs");
+
+var nodeModules = {};
+fs.readdirSync("node_modules")
+  .filter(function(x) {
+    return [".bin"].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = "commonjs " + mod;
+  });
 
 module.exports = {  
   entry: './index.ts',
@@ -18,5 +29,5 @@ module.exports = {
     ]
   },
   target: 'node',
-  externals: ['aws-sdk', 'node-pre-gyp']
+  externals: nodeModules,
 }
